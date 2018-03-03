b87493e8422cdf8e37db6cdc01f2aaa90b2f8cba
Понарьин Сергей <ponaryin@intaro.ru>
2018-03-03 23:27:25 +0300

    [ex1-7]

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -91,13 +91,15 @@
 
     <?if($APPLICATION->GetCurPage(true) != "/index.php"):?>
         <!-- breadcrumbs -->
-        <div class="breadcrumbs-box">
-            <div class="inner-wrap">
-                <a href="">Главная</a>
-                <a href="">Мебель</a>
-                <span>Выставки и события</span>
-            </div>
-        </div>
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:breadcrumb",
+            "breadcrumb",
+            Array(
+                "PATH" => "",
+                "SITE_ID" => "s1",
+                "START_FROM" => "0"
+            )
+        );?>
         <!-- /breadcrumbs -->
     <?endif;?>
 
